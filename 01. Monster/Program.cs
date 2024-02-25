e37c2eb6b8f0ee21766721d115e9ee1134d0b95d
Mihaela Hege <143254623+MGHege@users.noreply.github.com>
2024-02-25 12:56:14 +0200

    Add files via upload

---
+++ 01. Monster/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _01._Monster;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Queue<int> armorMonster = new Queue<int>(Console.ReadLine().Split(",", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray());
+        Stack<int> strenghtSoldeir = new Stack<int>(Console.ReadLine().Split(",", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray());
+        int monsterKill = 0;
+        while (armorMonster.Count > 0 && strenghtSoldeir.Count > 0)
+        {
+            int soldeir = strenghtSoldeir.Pop();
+            int monster = armorMonster.Dequeue();
+            if (soldeir >= monster)
+            {
+                monsterKill++;
+                if (soldeir - monster > 0)
+                {
+                    strenghtSoldeir.Push(soldeir - monster);
+                }
+
+            }
+            else
+            {
+                if (monster - soldeir > 0)
+                {
+                    armorMonster.Enqueue(monster - soldeir);
+                }
+            }
+
+        }
+        if (armorMonster.Count == 0)
+        {
+            Console.WriteLine($"All monsters have been killed!");
+        }
+        if (strenghtSoldeir.Count == 0)
+        {
+            Console.WriteLine($"The soldier has been defeated.");
+        }
+        Console.WriteLine($"Total monsters killed: {monsterKill}");
+    }
+}
+
---
+++ 01. MonsterExtermination/Program.cs
@@ -0,0 +1,47 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Queue<int> armorMonster = new Queue<int>(Console.ReadLine().Split(",", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+        Stack<int> strenghtSoldeir = new Stack<int>(Console.ReadLine().Split(",", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+        int monsterKill = 0;
+        while (armorMonster.Count > 0 && strenghtSoldeir.Count > 0)
+        {
+            int soldeir = strenghtSoldeir.Pop();
+            int monster = armorMonster.Dequeue();
+            if (soldeir >= monster)
+            {
+                monsterKill++;
+                if (soldeir - monster > 0)
+                {
+                    strenghtSoldeir.Push(soldeir - monster);
+                }
+
+            }
+            else
+            {
+                if (monster - soldeir > 0)
+                {
+                    armorMonster.Enqueue(monster - soldeir);
+                }
+            }
+
+        }
+        if (armorMonster.Count == 0)
+        {
+            Console.WriteLine($"All monsters have been killed!");
+        }
+        if (strenghtSoldeir.Count == 0)
+        {
+            Console.WriteLine($"The soldier has been defeated.");
+        }
+        Console.WriteLine($"Total monsters killed: {monsterKill}");
+    }
+}
+/*
+20,15,10
+5,15,10,25
+ 
+ 
+ 
+ */
