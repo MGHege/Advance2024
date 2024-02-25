cf71aafac8a21a013c1690808bf2f6219efe5e1f
Mihaela Hege <143254623+MGHege@users.noreply.github.com>
2024-02-25 12:44:01 +0200

    Add files via upload

---
+++ 01. BaristaContest/Program.cs
@@ -0,0 +1,88 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Queue<int> coffee = new Queue<int>(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+        Stack<int> milk = new Stack<int>(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+        Dictionary<int, string> drinks = new Dictionary<int, string>
+        {{50,"Cortado" },
+            {75,"Espresso"},
+            {100,  "Capuccino" },
+            {150,"Americano" },
+            {200, "Latte"},
+        };
+
+        Dictionary<string, int> coctel = new Dictionary<string, int>();
+        while (coffee.Count > 0 && milk.Count > 0)
+        {
+            int count = coffee.Peek() + milk.Peek();
+            foreach (var kvp in drinks)
+            {
+                if (kvp.Key == count)
+                {
+                    if (!coctel.ContainsKey(kvp.Value))
+                    {
+                        coctel.Add(kvp.Value, 0);
+                    }
+                    coctel[kvp.Value] += 1;
+                    milk.Pop();
+                }
+                
+            }
+            if (!drinks.ContainsKey(count))
+            {
+                milk.Push(milk.Pop() - 5);
+            }
+            coffee.Dequeue();
+        }
+
+        if (coffee.Count == 0 && milk.Count == 0)
+        {
+            Console.WriteLine($"Nina is going to win! She used all the coffee and milk!");
+            Console.WriteLine($"Coffee left: none");
+            Console.WriteLine($"Milk left: none");
+        }
+        else
+        {
+            Console.WriteLine($"Nina needs to exercise more! She didn't use all the coffee and milk!");
+            if (coffee.Any())
+            {
+                Console.WriteLine($"Coffee left: {string.Join(", ", coffee)}");
+            }
+            else
+            {
+                Console.WriteLine($"Coffee left: none");
+            }
+            if (milk.Any())
+            {
+                Console.WriteLine($"Milk left: {string.Join(", ", milk)}");
+            }
+            else
+            {
+                Console.WriteLine($"Milk left: none");
+
+            }
+
+        }
+        var coDrinks = coctel.OrderBy(x => x.Value).ThenByDescending(x => x.Key).ToDictionary(x => x.Key, x => x.Value);
+        foreach (var keyValue in coDrinks)
+        {
+            Console.WriteLine($"{keyValue.Key}: {keyValue.Value}");
+        }
+    }
+}
+/*
+20, 35, 100, 27, 56, 30, 30
+45, 20, 144, 173, 100, 40, 30
+
+20, 1, 10, 16, 1, 5
+205, 70, 30
+
+
+
+25, 50, 30
+50, 25
+
+
+
+**/
---
+++ SecoundExam/Program.cs
@@ -0,0 +1,142 @@
+﻿namespace SecoundExam;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        int n = int.Parse(Console.ReadLine());
+        char[,] matrix = new char[n, n];
+
+        int startR = -1;
+        int startC = -1;
+        int enemy = 0;
+        for (int row = 0; row < matrix.GetLength(0); row++)
+        {
+            string data = Console.ReadLine();
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                matrix[row, col] = data[col];
+                if (matrix[row, col] == 'J')
+                {
+                    startR = row;
+                    startC = col;
+                }
+                if (matrix[row, col] == 'E')
+                {
+                    enemy++;
+                }
+            }
+        }
+
+        int armor = 300;
+        string commands;
+        while ((commands = Console.ReadLine())!="")
+        {
+            string command = commands;
+            matrix[startR, startC] = '-';
+            if (command == "left")
+            {
+                startC--;
+            }
+            if (command == "right")
+            {
+                startC++;
+            }
+            if (command == "up")
+            {
+                startR--;
+            }
+            if (command == "down")
+            {
+                startR++;
+            }
+            if (matrix[startR, startC] != '-')
+            {
+                if (matrix[startR, startC] == 'E')
+                {
+                    matrix[startR, startC] = '-';
+                    armor -= 100;
+                    enemy--;//the last enemy??
+                    if (enemy==0)
+                    { Console.WriteLine($"Mission accomplished, you neutralized the aerial threat!");
+                        break;
+                    }
+                    if (armor==0&&enemy>0)
+                    {
+                        Console.WriteLine($"Mission failed, your jetfighter was shot down! Last coordinates [{startR}, {startC}]!");
+                        break;
+                    }
+                }
+                if (matrix[startR, startC] == 'R')
+                {
+                    armor = 300;
+                    matrix[startR, startC] = '-';
+                }
+            }
+           
+        }
+        matrix[startR, startC] = 'J';
+        
+        Output(matrix);
+    }
+
+    private static void Output(char[,] matrix)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int e = 0; e < matrix.GetLength(1); e++)
+            {
+                Console.Write(matrix[i, e]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+/*
+5
+-R---
+-J--E
+-E---
+--E-E
+-R---
+up
+down
+down
+down
+right
+right
+right
+ 
+4
+-R--
+-JEE
+-E-R
+--E-
+right
+right
+down
+left
+left
+down
+right
+
+5
+-J--E
+-R--E
+-E--R
+--R-E
+-R---
+right
+right
+right
+down
+down
+down
+left
+left
+left
+up
+ 
+ 
+ */
